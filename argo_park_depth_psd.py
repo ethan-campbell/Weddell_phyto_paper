cbdeb9beb322cf9c73afba50590d793915acb96e
Ethan Campbell <ethancc@uw.edu>
2019-10-01 11:15:01 -0700

    Fixed L-S method (changed from SciPy implementation to AstroPy)

--- argo_park_depth_psd.py
+++ argo_park_depth_psd.py
@@ -6,6 +6,7 @@ import pandas as pd
 import pandas.plotting._converter as pandacnv   # FIXME: only necessary due to Pandas 0.21.0 bug with Datetime plotting
 pandacnv.register()                             # FIXME: only necessary due to Pandas 0.21.0 bug with Datetime plotting
 from scipy import signal
+from astropy.timeseries import LombScargle
 import matplotlib.pyplot as plt
 
 # internal imports
@@ -15,7 +16,8 @@ import load_product as ldp
 data_dir = '/Users/Ethan/Documents/Research/2016-08 - UW/Data/Argo/Drift/'
 output_dir = '/Users/Ethan/Documents/Research/Git/Weddell_polynya/Temporary results/'
 
-# load data
+######### load APEX data #########
+
 wmoid = 5904471
 drift_data = ldp.argo_drift_data(data_dir,wmoid,status='R',bad_pres_range=(500,1500))
 drift_pres = pd.Series(index=drift_data['datetime'],data=drift_data['pres'])
@@ -23,27 +25,28 @@ drift_temp = pd.Series(index=drift_data['datetime'],data=drift_data['temp'])
 t = (drift_temp.index - drift_temp.index[0]).astype('timedelta64[m]') / (24 * 60)  # units: days
 y = drift_temp.values.copy()
 
-######### analysis #########
+######### analyze APEX data #########
 
 f = 60 * 60 * 24 * (2 * 7.2921e-5 * sin(2 * pi * 65 / 360) / (2 * pi))  # f at 65°
 
 # Lomb-Scargle method
-freq = linspace(0.1,2.25,100000)
-pgram = signal.lombscargle(t,y,freq)
-pgram_6hr = signal.lombscargle(t[::6],y[::6],freq)
+ls_freq = linspace(0.01,3.0,10000)
+Pxx_den = LombScargle(t,y,normalization='psd').power(ls_freq)
+Pxx_den_6hr = LombScargle(t[::6],y[::6],normalization='psd').power(ls_freq)
 
 # plot
 plt.figure(figsize=(6,8))
 plt.subplot(2,1,1)
-plt.semilogy(freq,signal.medfilt(pgram,kernel_size=1001),'k',label='Hourly')
-plt.semilogy(freq,signal.medfilt(pgram_6hr,kernel_size=1001),'b',label='6-hourly subsampled')
-plt.xlim([0.1,2.25])
+plt.semilogy(ls_freq,signal.medfilt(Pxx_den,kernel_size=501),'k',label='Hourly')
+plt.semilogy(ls_freq,signal.medfilt(Pxx_den_6hr,kernel_size=501),'b',label='6-hourly subsampled')
+plt.xlim([0.1,2.5])
+plt.ylim([5e-7,8e-3])
 old_ylim = plt.ylim()
-plt.plot([f,f],old_ylim,'k--',label=r'$f$')
+plt.plot([f,f],old_ylim,'k--',label=r'$f$ at 65°S')
 plt.ylim(old_ylim)
-# plt.text(f,10**-4,r'$f$',fontsize=18)
 plt.legend(frameon=False)
 plt.xlabel('Frequency (cpd)')
+plt.ylabel(r'Power (°C$^2$/cpd)')
 plt.title('Lomb-Scargle method')
 
 # Welch's method
@@ -54,13 +57,31 @@ welch_freq_6hr,Pxx_den_6hr = signal.welch(y[::6],4,nperseg=256,detrend='linear',
 plt.subplot(2,1,2)
 plt.semilogy(welch_freq,Pxx_den,'k',label='Hourly')
 plt.semilogy(welch_freq_6hr,Pxx_den_6hr,'b',label='6-hourly subsampled')
-plt.xlim([0.1,2.25])
+plt.xlim([0.1,2.5])
+plt.ylim([5e-7,8e-3])
 old_ylim = plt.ylim()
-plt.plot([f,f],old_ylim,'k--',label=r'$f$')
+plt.plot([f,f],old_ylim,'k--',label=r'$f$ at 65°S')
 plt.ylim(old_ylim)
 plt.legend(frameon=False)
 plt.xlabel('Frequency (cpd)')
+plt.ylabel(r'Power (°C$^2$/cpd)')
 plt.title('Welch\'s method')
 plt.tight_layout()
 
 plt.savefig(output_dir + '5904471_park_depth_temp_psd.pdf')
+
+
+######### load Navis data #########
+
+# pd.read_excel()
+#
+# t = ((pd.to_datetime(matt_data['Date']) - pd.to_datetime(matt_data['Date'])[0]).values.astype('timedelta64[m]')
+#      / timedelta64(1,'m')) / (24 * 60)
+# y = matt_data['Temp'].values
+#
+# t = t[~isnan(y)]
+# y = y[~isnan(y)]
+#
+# freq = linspace(0.1,2.25,10000)
+# pgram = signal.lombscargle(t,y,freq)
+# plt.semilogy(freq,signal.medfilt(pgram,kernel_size=101),'k',label='Hourly')
