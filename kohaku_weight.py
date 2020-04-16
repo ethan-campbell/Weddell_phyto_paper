12bf0964034c2dcb3e85218191a293d0165d7d4b
Ethan Campbell <ethancc@uw.edu>
2020-04-16 03:48:04 -0700

    Kohaku update

--- kohaku_weight.py
+++ kohaku_weight.py
@@ -10,7 +10,7 @@ os.chdir('/Users/Ethan/Documents/Emi/Kohaku the cat/2020-02-26 - FIP journey plo
 data = pd.read_excel('Kohaku weight.xlsx',header=1)
 # data['Date'] = pd.DatetimeIndex(data['Date'])
 
-plt.figure(figsize=(8,5))
+plt.figure(figsize=(9,5))
 plt.plot(data['Date'].values,data['Weight (lbs)'].values,c='k',lw=1,zorder=2)
 plt.scatter(data['Date'].values,data['Weight (lbs)'].values,c='k',s=8,zorder=3)
 plt.gcf().autofmt_xdate()
@@ -21,9 +21,9 @@ plt.gca().axvspan(xmin=datetime(2019,12,18),xmax=datetime(2020,1,5),
                   ymin=0,ymax=1,facecolor='goldenrod',alpha=0.3,zorder=1,label='Shelter #2')
 plt.gca().axvspan(xmin=datetime(2020,1,5),xmax=datetime(2020,1,5,12,0,0),
                   ymin=0,ymax=1,facecolor='maroon',alpha=0.7,zorder=1,label='Adopted')
-plt.gca().axvspan(xmin=datetime(2020,1,29),xmax=datetime(2020,2,28),
+plt.gca().axvspan(xmin=datetime(2020,1,29),xmax=datetime(2020,3,31),
                   ymin=0,ymax=1,facecolor='0.5',alpha=0.3,zorder=1,label='On GS')
-plt.xlim([datetime(2019,12,1),datetime(2020,2,27)])
+plt.xlim([datetime(2019,12,1),datetime(2020,4,1)])
 plt.grid(alpha=0.5)
 plt.legend(loc='lower left')
 plt.title('Kohaku\'s FIP journey')
